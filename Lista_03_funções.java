b0be66d8bdfcc528f494803c7ab62cdbb445f539
GustavoZen <112349322+GustavoZen@users.noreply.github.com>
2022-09-09 13:43:54 -0300

    Update Lista_03_funções.java

--- Lista_03_funções.java
+++ Lista_03_funções.java
@@ -3,6 +3,32 @@ package lista_03;
 import java.util.Scanner;
 
 public class Lista_03_funções {
+	static Scanner entrada = new Scanner(System.in);
+
+	public static int Somatorioimparvetor(int vet[]) {
+		int saida = 0;
+		for (int i = 1; i < vet.length; i+=2) {
+			saida += vet[i];
+		}
+		return saida;
+	}
+
+	public static void povoarvetor(int[] vetor, int num) {
+		if (vetor.length == 0) {
+			return;
+		}
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.println("Informe o " + (i + 1) + "º elemento da array " + num + ".");
+			vetor[i] = entrada.nextInt();
+		}
+	}
+
+	public static int maior(int n, int m) {
+		if (n > m) {
+			return n;
+		} else
+			return m;
+	}
 
 	public static void mostrar_vetor(int vetor[]) {
 		for (int i = 0; i < vetor.length; i++) {
@@ -91,29 +117,16 @@ public class Lista_03_funções {
 		return vetor;
 	}
 
-	public static int[] Inversao_de_vetores_07(int[] vetor1, int[] vetor2) {
-		int vetorauxiliar1[];
-		int vetorauxiliar2[];
-		vetorauxiliar1 = new int[vetor1.length];
-		vetorauxiliar2 = new int[vetor2.length];
-		for (int i = 0; i < vetor1.length; i++) {
-			vetorauxiliar1[i] = vetor1[i];
+	public static void Inversao_de_vetores_07(int[] vetor1, int[] vetor2) {
+		int vetorauxiliar[] = new int[(vetor1.length + vetor2.length) - maior(vetor1.length, vetor2.length)];
+		for (int i = 0; i < vetorauxiliar.length; i++) {
+			vetorauxiliar[i] = vetor1[i];
+			vetor1[i] = vetor2[i];
+			vetor2[i] = vetorauxiliar[i];
 		}
-		for (int i = 0; i < vetor2.length; i++) {
-			vetorauxiliar2[i] = vetor2[i];
-		}
-		vetor2 = new int[vetorauxiliar1.length];
-		vetor1 = new int[vetorauxiliar2.length];
-		for (int i = 0; i < vetorauxiliar1.length; i++) {
-			vetor2[i] = vetorauxiliar1[i];
-		}
-		for (int i = 0; i < vetorauxiliar2.length; i++) {
-			vetor1[i] = vetorauxiliar2[i];
-		}
-		return vetor1;
 	}
 
-	public static void Priorizacao_08(int[] vetor1, int[] vetor2) {
+	public static int[] Priorizacao_08(int[] vetor1, int[] vetor2) {
 		int media = Somatorio_04(vetor2) / vetor2.length;
 		int add = 0, aux = 0;
 		int vetoraux[] = new int[vetor2.length];
@@ -139,30 +152,86 @@ public class Lista_03_funções {
 				}
 			}
 		}
+		return vetor2;
+	}
+
+	public static int[] Compactacao(int vet[], int n) {
+		int inicial = vet[0], cont = 0, aux = 0;
+		int[] vet2;
+		for (int i = 0; i < vet.length; i++) {
+			if (vet[i] != 1 && vet[i] != 0)
+				System.out.println("O vetor deve ser composto apenas de \"0's\" e \"1'\"s");
+			if (vet[i] != inicial % 2)
+				inicial++;
+		}
+		if (vet[0] == 0) {
+			vet2 = new int[(inicial + 1) * 2];
+		} else
+			vet2 = new int[inicial * 2];
+		inicial = vet[0];
+		for (int i = 0; i < vet.length; i++) {
+			if (vet[i] != inicial % 2) {
+				vet2[aux] = vet[i - 1];
+				vet2[++aux] = cont;
+				aux++;
+				cont = 0;
+				inicial++;
+			}
+			cont++;
+			if (i == vet.length - 1) {
+				vet2[aux] = vet[i];
+				vet2[++aux] = cont;
+			}
+		}
+		for (int i : vet2) {
+			System.out.print(i + ",");
+		}
+		return vet2;
+	}
+
+	public static int[] Descompactacao(int vet[],int n) {
+		int vet2[] = new int[Somatorioimparvetor(vet)];
+		int cont = 0;
+		for (int i = 0; i < vet.length; ) {
+			for (int j = 0; j < vet[i + 1]; j++) {
+				vet2[cont + j] = vet[i];
+			}
+			cont += vet[i+1];
+			i+= cont;
+		}
+		return vet2;
 	}
 
 	public static void main(String[] args) {
+		int vetor2[], vetor1[], vetor3[];
+		int n = 0;
+		int tam1 = 0, tam2 = 0,tam3 = 0;
+		do {
+			System.out.println("Quantos vetores serão utilizados? (máx: 2;mín: 1)");
+			n = entrada.nextInt();
+			if (n != 1 && n != 2) {
+				System.out.println("Digite uma entrada válida!");
+			}
+		} while (n != 1 && n != 2);
+		if (n == 1) {
+			System.out.println("Informe o tamanho do vetor1: ");
+			tam1 = entrada.nextInt();
+		} else if (n == 2) {
+			System.out.println("Informe o tamanho do vetor1: ");
+			tam1 = entrada.nextInt();
+			System.out.println("Informe o tamanho do vetor2: ");
+			tam2 = entrada.nextInt();
+		}
 
-		int vetor2[];
-		int vetor1[];
-		System.out.println("Informe o tamanho do vetor1: ");
-		Scanner entrada = new Scanner(System.in);
-		int n = entrada.nextInt();
-		vetor1 = new int[n];
-		for (int i = 0; i < n; i++) {
-			System.out.println("Informe o " + (i + 1) + "º elemento da 1ª array.");
-			vetor1[i] = entrada.nextInt();
-		}
-		System.out.println("Informe o tamanho do vetor2: ");
-		n = entrada.nextInt();
-		vetor2 = new int[n];
-		for (int i = 0; i < n; i++) {
-			System.out.println("Informe o " + (i + 1) + "º elemento da 2ª array.");
-			vetor2[i] = entrada.nextInt();
-		}
-		Priorizacao_08(vetor1, vetor2);
-		mostrar_vetor(vetor1);
-		mostrar_vetor(vetor2);
+		vetor1 = new int[tam1];
+		if (tam1 != 0)
+			povoarvetor(vetor1, 1);
+		vetor2 = new int[tam2];
+		if (tam2 != 0)
+			povoarvetor(vetor2, 2);
 		entrada.close();
+		vetor3 = new int [tam3];
+		vetor3 = Descompactacao(vetor1,tam3);
+		mostrar_vetor(vetor3);
 	}
 }
